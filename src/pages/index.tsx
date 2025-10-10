8ad88a8469f2f73d6d012372d37125b049431cc4
Viralink-Goddard <avidproductsint@gmail.com>
2025-10-10 13:45:57 -0400

    Create index.tsx

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+// src/pages/index.tsx
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui", padding: 24 }}>
+      <h1>Viralink Pro</h1>
+      <p>Home page loads 🎉</p>
+    </div>
+  );
+}
