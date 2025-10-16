d61abcb20396283d90171055438677926035aff0
Viralink-Goddard <avidproductsint@gmail.com>
2025-10-16 13:41:14 -0400

    Create Dashboard.tsx

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div style={{ fontFamily: "system-ui", padding: 24 }}>
+      <h1>Dashboard</h1>
+      <p>Placeholder page</p>
+    </div>
+  );
+}
