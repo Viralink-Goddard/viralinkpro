15264d6ba63d00312590f023dbee52138471a7c6
Viralink-Goddard <avidproductsint@gmail.com>
2025-10-16 13:42:21 -0400

    Create Analytics.tsx

---
+++ src/pages/Analytics.tsx
@@ -0,0 +1,8 @@
+xexport default function Page() {
+  return (
+    <div style={{ fontFamily: "system-ui", padding: 24 }}>
+      <h1>Analytics</h1>
+      <p>Placeholder page</p>
+    </div>
+  );
+}
